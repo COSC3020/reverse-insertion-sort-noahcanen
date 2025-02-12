c704b2b6426fe58c19f09a2cb14e44a5a29a9437
noahcanen <ncanen@uwyo.edu>
2025-02-12 15:29:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function insertionSortReverse(array) {
-      for(var i = array.length-1; 0 < i; i--) {
+      for(var i = array.length-1; -1 < i; i--) {
       var val = array[i];
       var j;
       for(j = i; j <= array.length-1  && array[j+1] < val; j++) {
